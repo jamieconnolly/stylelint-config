636b5bf958fcf2d830fa04b86a35362ecb3b33a4
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-02-23 11:58:23 +0000

    :white_check_mark: Add in some basic tests using Jest

---
+++ __tests__/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var config = require('../'),
+    stylelint = require('stylelint');
+
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null;
+}
+
+it('test basic properties of config', function() {
+  expect(isObject(config.rules)).toBeTruthy();
+});
+
+it('load config in stylelint to validate rule syntax', function() {
+  return stylelint.lint({
+    code: 'a { font-weight: bold; }',
+    config: config,
+  }).then(function(data) {
+    expect(data).toBeTruthy();
+    expect(data.errored).toBeFalsy();
+    expect(data.results[0].warnings.length).toBe(0);
+  });
+});

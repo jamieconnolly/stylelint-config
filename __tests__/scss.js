95eae6eea260c33ff53cebf147a633511c850a8a
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-02-27 14:24:08 +0000

    :heavy_plus_sign: Add in stylelint-scss

---
+++ __tests__/scss.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var config = require('../scss'),
+    stylelint = require('stylelint');
+
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null;
+}
+
+it('test basic properties of config', function() {
+  expect(isObject(config.extends)).toBeTruthy();
+  expect(isObject(config.plugins)).toBeTruthy();
+  expect(isObject(config.rules)).toBeTruthy();
+});
+
+it('load config in stylelint to validate rule syntax', function() {
+  return stylelint.lint({
+    code: 'a { font-weight: bold; }\n',
+    config: config,
+  }).then(function(data) {
+    expect(data).toBeTruthy();
+    expect(data.errored).toBeFalsy();
+    expect(data.results[0].warnings.length).toBe(0);
+  });
+});

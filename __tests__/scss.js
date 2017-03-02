608eb22fc31cefe57a44f1d4d0bd92878e62faee
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-03-02 17:20:14 +0000

    :hammer: Update the scss preset tests to match the default

--- __tests__/scss.js
+++ __tests__/scss.js
@@ -1,23 +1,13 @@
 'use strict';
 
-var config = require('../scss');
+var path = require('path');
 var stylelint = require('stylelint');
-
-function isObject(obj) {
-  return typeof obj === 'object' && obj !== null;
-}
-
-it('test basic properties of config', function() {
-  expect(config.extends).toBeTruthy();
-  expect(isObject(config.plugins)).toBeTruthy();
-  expect(isObject(config.rules)).toBeTruthy();
-});
 
 it('load config in stylelint to validate rule syntax', function() {
   return stylelint.lint({
     code: 'a { font-weight: bold; }\n',
     codeFilename: 'valid.scss',
-    config: config,
+    configFile: path.join(__dirname, '../scss.js'),
   }).then(function(data) {
     expect(data).toBeTruthy();
     expect(data.errored).toBeFalsy();

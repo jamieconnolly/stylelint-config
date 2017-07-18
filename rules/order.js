af5322c42ae2330c6d561f0288bf939e198d7190
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-07-18 18:43:41 +0100

    :wrench: Split the rules into smaller files

---
+++ rules/order.js
@@ -0,0 +1,293 @@
+'use strict';
+
+module.exports = {
+  plugins: [
+    'stylelint-order',
+  ],
+  rules: {
+    // specify the order of content within declaration blocks
+    'order/order': [
+      'custom-properties',
+      'dollar-variables',
+      'at-variables',
+      'declarations',
+      'rules',
+      'at-rules',
+      'less-mixins',
+    ],
+
+    // specify the almost strict order of properties within declaration blocks
+    'order/properties-order': [
+      [
+        // Content
+        {
+          properties: [
+            'content',
+          ],
+        },
+        // Position
+        {
+          properties: [
+            'position',
+            'z-index',
+            'top',
+            'right',
+            'bottom',
+            'left',
+          ],
+        },
+        // Display
+        {
+          properties: [
+            'display',
+            'opacity',
+            'visibility',
+          ],
+        },
+        // Layout
+        {
+          properties: [
+            'flex',
+            'flex-basis',
+            'flex-direction',
+            'flex-flow',
+            'flex-grow',
+            'flex-shrink',
+            'flex-wrap',
+            'align-content',
+            'align-items',
+            'align-self',
+            'justify-content',
+            'order',
+
+            'grid',
+            'grid-area',
+            'grid-auto-columns',
+            'grid-auto-flow',
+            'grid-auto-rows',
+            'grid-column',
+            'grid-column-end',
+            'grid-column-gap',
+            'grid-column-start',
+            'grid-gap',
+            'grid-row',
+            'grid-row-end',
+            'grid-row-gap',
+            'grid-row-start',
+            'grid-template',
+            'grid-template-areas',
+            'grid-template-columns',
+            'grid-template-rows',
+
+            'float',
+            'clear',
+          ],
+        },
+        // Clipping
+        {
+          properties: [
+            'overflow',
+            'overflow-x',
+            'overflow-y',
+
+            'clip',
+            'clip-path',
+            'zoom',
+          ],
+        },
+        // Box
+        {
+          properties: [
+            'box-sizing',
+
+            'width',
+            'min-width',
+            'max-width',
+            'height',
+            'min-height',
+            'max-height',
+
+            'margin',
+            'margin-top',
+            'margin-right',
+            'margin-bottom',
+            'margin-left',
+
+            'padding',
+            'padding-top',
+            'padding-right',
+            'padding-bottom',
+            'padding-left',
+          ],
+        },
+        // Element-specific
+        {
+          properties: [
+            'columns',
+            'column-count',
+            'column-fill',
+            'column-gap',
+            'column-rule',
+            'column-rule-color',
+            'column-rule-style',
+            'column-rule-width',
+            'column-span',
+            'column-width',
+
+            'table-layout',
+            'empty-cells',
+            'caption-side',
+            'border-spacing',
+            'border-collapse',
+
+            'list-style',
+            'list-style-image',
+            'list-style-position',
+            'list-style-type',
+          ],
+        },
+        // Border
+        {
+          properties: [
+            'border',
+            'border-color',
+            'border-image',
+            'border-image-outset',
+            'border-image-repeat',
+            'border-image-slice',
+            'border-image-source',
+            'border-image-width',
+            'border-radius',
+            'border-style',
+            'border-width',
+
+            'border-top',
+            'border-top-color',
+            'border-top-left-radius',
+            'border-top-right-radius',
+            'border-top-style',
+            'border-top-width',
+
+            'border-right',
+            'border-right-color',
+            'border-right-style',
+            'border-right-width',
+
+            'border-bottom',
+            'border-bottom-color',
+            'border-bottom-left-radius',
+            'border-bottom-right-radius',
+            'border-bottom-style',
+            'border-bottom-width',
+
+            'border-left',
+            'border-left-color',
+            'border-left-style',
+            'border-left-width',
+
+            'outline',
+            'outline-color',
+            'outline-offset',
+            'outline-style',
+            'outline-width',
+          ],
+        },
+        // Background
+        {
+          properties: [
+            'background',
+            'background-attachment',
+            'background-blend-mode',
+            'background-clip',
+            'background-color',
+            'background-image',
+            'background-origin',
+            'background-position',
+            'background-repeat',
+            'background-size',
+
+            'box-shadow',
+            'fill',
+          ],
+        },
+        // Text
+        {
+          properties: [
+            'font',
+            'font-family',
+            'font-feature-settings',
+            'font-kerning',
+            'font-size',
+            'font-size-adjust',
+            'font-smoothing',
+            'font-stretch',
+            'font-style',
+            'font-variant',
+            'font-weight',
+
+            'letter-spacing',
+            'line-height',
+
+            'color',
+
+            'text-align',
+            'text-align-last',
+            'text-decoration',
+            'text-decoration-color',
+            'text-decoration-line',
+            'text-decoration-style',
+            'text-emphasis',
+            'text-emphasis-color',
+            'text-emphasis-position',
+            'text-emphasis-style',
+            'text-indent',
+            'text-justify',
+            'text-overflow',
+            'text-rendering',
+            'text-shadow',
+            'text-transform',
+            'text-wrap',
+
+            'hyphens',
+            'vertical-align',
+            'white-space',
+            'word-break',
+            'word-spacing',
+            'word-wrap',
+          ],
+        },
+        // Transform
+        {
+          properties: [
+            'transition',
+            'transition-delay',
+            'transition-duration',
+            'transition-property',
+            'transition-timing-function',
+
+            'transform',
+            'transform-origin',
+            'transform-style',
+
+            'perspective',
+            'perspective-origin',
+
+            'backface-visibility',
+
+            'animation',
+            'animation-delay',
+            'animation-direction',
+            'animation-duration',
+            'animation-fill-mode',
+            'animation-iteration-count',
+            'animation-name',
+            'animation-play-state',
+            'animation-timing-function',
+          ],
+        },
+      ], {
+        unspecified: 'bottomAlphabetical',
+      },
+    ],
+  },
+};
---
+++ rules/stylistic-issues.js
@@ -0,0 +1,280 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    // require or disallow an empty line before at-rules
+    'at-rule-empty-line-before': ['always', {
+      except: ['blockless-after-same-name-blockless', 'first-nested'],
+      ignore: ['after-comment'],
+    }],
+
+    // specify lowercase or uppercase for at-rules names
+    'at-rule-name-case': 'lower',
+
+    // require a newline after at-rule names
+    'at-rule-name-newline-after': 'always-multi-line',
+
+    // require a single space after at-rule names
+    'at-rule-name-space-after': 'always-single-line',
+
+    // require a newline after the semicolon of at-rules
+    'at-rule-semicolon-newline-after': 'always',
+
+    // require a single space or disallow whitespace before the semicolons of at rules
+    'at-rule-semicolon-space-before': 'never',
+
+    // require or disallow an empty line before the closing brace of blocks
+    'block-closing-brace-empty-line-before': 'never',
+
+    // require a newline or disallow whitespace after the closing brace of blocks
+    'block-closing-brace-newline-after': 'always',
+
+    // require a newline or disallow whitespace before the closing brace of blocks
+    'block-closing-brace-newline-before': 'always-multi-line',
+
+    // require a single space or disallow whitespace after the closing brace of blocks
+    'block-closing-brace-space-after': null,
+
+    // require a single space or disallow whitespace before the closing brace of blocks
+    'block-closing-brace-space-before': 'always-single-line',
+
+    // require a newline after the opening brace of blocks
+    'block-opening-brace-newline-after': 'always-multi-line',
+
+    // require a newline or disallow whitespace before the opening brace of blocks
+    'block-opening-brace-newline-before': null,
+
+    // require a single space or disallow whitespace after the opening brace of blocks
+    'block-opening-brace-space-after': 'always-single-line',
+
+    // require a single space or disallow whitespace before the opening brace of blocks
+    'block-opening-brace-space-before': 'always',
+
+    // specify lowercase or uppercase for hex colors
+    'color-hex-case': 'lower',
+
+    // specify short or long notation for hex colors
+    'color-hex-length': 'short',
+
+    // require or disallow an empty line before comments
+    'comment-empty-line-before': ['always', {
+      except: ['first-nested'],
+      ignore: ['stylelint-commands'],
+    }],
+
+    // require or disallow whitespace on the inside of comment markers
+    'comment-whitespace-inside': 'always',
+
+    // require or disallow an empty line before custom properties
+    'custom-property-empty-line-before': ['always', {
+      except: ['after-comment', 'after-custom-property', 'first-nested'],
+      ignore: ['inside-single-line-block'],
+    }],
+
+    // require a single space or disallow whitespace after the bang of declarations
+    'declaration-bang-space-after': 'never',
+
+    // require a single space or disallow whitespace before the bang of declarations
+    'declaration-bang-space-before': 'always',
+
+    // require a newline or disallow whitespace after the semicolons of declaration blocks
+    'declaration-block-semicolon-newline-after': 'always-multi-line',
+
+    // require a newline or disallow whitespace before the semicolons of declaration blocks
+    'declaration-block-semicolon-newline-before': 'never-multi-line',
+
+    // require a single space or disallow whitespace after the semicolons of declaration blocks
+    'declaration-block-semicolon-space-after': 'always-single-line',
+
+    // require a single space or disallow whitespace before the semicolons of declaration blocks
+    'declaration-block-semicolon-space-before': 'never',
+
+    // require or disallow a trailing semicolon within declaration blocks
+    'declaration-block-trailing-semicolon': 'always',
+
+    // require a newline or disallow whitespace after the colon of declarations
+    'declaration-colon-newline-after': 'always-multi-line',
+
+    // require a single space or disallow whitespace after the colon of declarations
+    'declaration-colon-space-after': 'always-single-line',
+
+    // require a single space or disallow whitespace before the colon of declarations
+    'declaration-colon-space-before': 'never',
+
+    // require or disallow an empty line before declarations
+    'declaration-empty-line-before': ['never', {
+      ignore: ['after-declaration', 'inside-single-line-block'],
+    }],
+
+    // specify whether or not quotation marks should be used around font family names
+    'font-family-name-quotes': 'always-where-recommended',
+
+    // require numeric or named (where possible) font-weight values
+    'font-weight-notation': 'numeric',
+
+    // require a newline or disallow whitespace after the commas of functions
+    'function-comma-newline-after': 'always-multi-line',
+
+    // require a newline or disallow whitespace before the commas of functions
+    'function-comma-newline-before': null,
+
+    // require a single space or disallow whitespace after the commas of functions
+    'function-comma-space-after': 'always-single-line',
+
+    // require a single space or disallow whitespace before the commas of functions
+    'function-comma-space-before': 'never',
+
+    // limit the number of adjacent empty lines within functions
+    'function-max-empty-lines': 0,
+
+    // specify lowercase or uppercase for function names
+    'function-name-case': 'lower',
+
+    // require a newline or disallow whitespace on the inside of the parentheses of functions
+    'function-parentheses-newline-inside': 'always-multi-line',
+
+    // require a single space or disallow whitespace on the inside of the parentheses of functions
+    'function-parentheses-space-inside': 'never-single-line',
+
+    // require or disallow quotes for urls
+    'function-url-quotes': 'always',
+
+    // require or disallow whitespace after functions
+    'function-whitespace-after': 'always',
+
+    // specify indentation
+    'indentation': 2,
+
+    // disallow units for zero lengths
+    'length-zero-no-unit': true,
+
+    // limit the number of adjacent empty lines
+    'max-empty-lines': 1,
+
+    // limit the length of a line
+    'max-line-length': [119, {ignore: 'non-comments'}],
+
+    // require a single space or disallow whitespace after the colon in media features
+    'media-feature-colon-space-after': 'always',
+
+    // require a single space or disallow whitespace before the colon in media features
+    'media-feature-colon-space-before': 'never',
+
+    // specify lowercase or uppercase for media feature names
+    'media-feature-name-case': 'lower',
+
+    // require a single space or disallow whitespace on the inside of the parentheses within media features
+    'media-feature-parentheses-space-inside': 'never',
+
+    // require a single space or disallow whitespace after the range operator in media features
+    'media-feature-range-operator-space-after': 'always',
+
+    // require a single space or disallow whitespace before the range operator in media features
+    'media-feature-range-operator-space-before': 'always',
+
+    // require a newline or disallow whitespace after the commas of media query lists
+    'media-query-list-comma-newline-after': 'always-multi-line',
+
+    // require a newline or disallow whitespace before the commas of media query lists
+    'media-query-list-comma-newline-before': 'never-multi-line',
+
+    // require a single space or disallow whitespace after the commas of media query lists
+    'media-query-list-comma-space-after': 'always-single-line',
+
+    // require a single space or disallow whitespace before the commas of media query lists
+    'media-query-list-comma-space-before': 'never',
+
+    // disallow end-of-line whitespace
+    'no-eol-whitespace': true,
+
+    // disallow missing end-of-source newlines
+    'no-missing-end-of-source-newline': true,
+
+    // require or disallow a leading zero for fractional numbers less than 1
+    'number-leading-zero': 'always',
+
+    // disallow trailing zeros in numbers
+    'number-no-trailing-zeros': true,
+
+    // specify lowercase or uppercase for properties
+    'property-case': 'lower',
+
+    // require or disallow an empty line before rules
+    'rule-empty-line-before': ['always-multi-line', {
+      except: ['first-nested'],
+      ignore: ['after-comment'],
+    }],
+
+    // require a single space or disallow whitespace on the inside of the brackets within attribute selectors
+    'selector-attribute-brackets-space-inside': 'never',
+
+    // require a single space or disallow whitespace after operators within attribute selectors
+    'selector-attribute-operator-space-after': 'never',
+
+    // require a single space or disallow whitespace before operators within attribute selectors
+    'selector-attribute-operator-space-before': 'never',
+
+    // require or disallow quotes for attribute values
+    'selector-attribute-quotes': 'always',
+
+    // require a single space or disallow whitespace after the combinators of selectors
+    'selector-combinator-space-after': 'always',
+
+    // require a single space or disallow whitespace before the combinators of selectors
+    'selector-combinator-space-before': 'always',
+
+    // disallow non-space characters for descendant combinators of selectors
+    'selector-descendant-combinator-no-non-space': true,
+
+    // require a newline or disallow whitespace after the commas of selector lists
+    'selector-list-comma-newline-after': 'always',
+
+    // require a newline or disallow whitespace before the commas of selector lists
+    'selector-list-comma-newline-before': null,
+
+    // require a single space or disallow whitespace after the commas of selector lists
+    'selector-list-comma-space-after': 'always-single-line',
+
+    // require a single space or disallow whitespace before the commas of selector lists
+    'selector-list-comma-space-before': 'never',
+
+    // specify lowercase or uppercase for pseudo-class selectors
+    'selector-pseudo-class-case': 'lower',
+
+    // require a single space or disallow whitespace on the inside of the parentheses within pseudo-class selectors
+    'selector-pseudo-class-parentheses-space-inside': 'never',
+
+    // specify lowercase or uppercase for pseudo-element selectors
+    'selector-pseudo-element-case': 'lower',
+
+    // specify single or double colon notation for applicable pseudo-elements
+    'selector-pseudo-element-colon-notation': 'double',
+
+    // specify lowercase or uppercase for type selector
+    'selector-type-case': 'lower',
+
+    // specify single or double quotes around strings
+    'string-quotes': 'double',
+
+    // specify lowercase or uppercase for units
+    'unit-case': 'lower',
+
+    // specify lowercase or uppercase for keywords values
+    'value-keyword-case': 'lower',
+
+    // require a newline or disallow whitespace after the commas of value lists
+    'value-list-comma-newline-after': 'always-multi-line',
+
+    // require a newline or disallow whitespace before the commas of value lists
+    'value-list-comma-newline-before': null,
+
+    // require a single space or disallow whitespace after the commas of value lists
+    'value-list-comma-space-after': 'always-single-line',
+
+    // require a single space or disallow whitespace before the commas of value lists
+    'value-list-comma-space-before': 'never',
+
+    // limit the number of adjacent empty lines within value lists
+    'value-list-max-empty-lines': 0,
+  },
+};

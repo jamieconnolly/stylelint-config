fa002ad7ab232a0f9d14796bb8c229f555fec5a6
Jamie Connolly <jamie@jamieconnolly.com>
2017-07-28 16:05:42 +0100

    :fire: Remove the tests that were looking for the scss preset

--- __tests__/basic.js
+++ __tests__/basic.js
@@ -8,11 +8,3 @@ it('test basic properties of config', function() {
   expect(isPlainObj(config)).toBe(true);
   expect(config.extends).toBeTruthy();
 });
-
-it('test basic properties of scss config', function() {
-  var config = require('../scss.js');
-
-  expect(isPlainObj(config)).toBe(true);
-  expect(config.extends).toBeTruthy();
-  expect(config.rules).toBeTruthy();
-});
--- __tests__/validate-config.js
+++ __tests__/validate-config.js
@@ -13,15 +13,3 @@ it('load config in stylelint to validate rule syntax', function() {
     expect(data.results[0].warnings.length).toBe(0);
   });
 });
-
-it('load config in stylelint to validate scss rule syntax', function() {
-  return new stylelint.lint({
-    code: 'a { font-weight: 500; }\n',
-    codeFilename: 'valid.scss',
-    configFile: 'scss.js',
-  }).then(function(data) {
-    expect(data).toBeTruthy();
-    expect(data.errored).toBeFalsy();
-    expect(data.results[0].warnings.length).toBe(0);
-  });
-});

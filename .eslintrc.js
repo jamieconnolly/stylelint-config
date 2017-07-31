cf0400de20c81fc7c12864125f7181b6d1cea922
Jamie Connolly <jamie@jamieconnolly.com>
2017-07-31 11:39:58 +0100

    :truck: Use .eslintrc.js instead of .eslintrc.json

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  env: {
+    'jest/globals': true,
+    'node': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:node/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: [
+    'jest',
+    'node',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'indent': ['error', 2],
+    'linebreak-style': 'error',
+    'quote-props': ['error', 'consistent-as-needed'],
+    'quotes': ['error', 'single', {avoidEscape: true}],
+    'semi': ['error', 'always'],
+    'sort-keys': 'error',
+  },
+};
